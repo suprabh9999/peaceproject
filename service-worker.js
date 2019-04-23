cf06d7a1e8d7ae59e362a741fcf1a2184e8f5d6d
Mona Lisa <official.suprabh@gmail.com>
2019-04-24 01:04:13 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/peaceproject/precache-manifest.6f41998a9c944607f6b425b2720af361.js"
+  "/precache-manifest.fce8d1b98acd4879d6866d7b14e9df2e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/peaceproject/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
